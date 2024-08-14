0c5622eecc3647e661a3c15952d12a14a981aed4
rn0x <rn0x.me@gmail.com>
2024-08-14 12:21:57 +0300

    Fix error handling middleware and improve error messages. Clean up and organize code.

--- src/index.js
+++ src/index.js
@@ -15,37 +15,37 @@ import path from 'path';
 
 dotenv.config();
 
-
 const app = express();
 const PORT = process.env.PORT || 7000;
 const root = path.resolve(process.cwd());
 
-// Routes Files
+// Serve static files from the 'files' directory
 app.use('/api/files', fileRoutes);
 
+// Serve the main HTML file at the root endpoint
 app.get('/', (req, res) => {
-  res.sendFile(path.join(root, '/index.html'));
+  res.sendFile(path.join(root, 'index.html'));
 });
 
 // Catch-all route for undefined routes
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
+  // Log the error stack to the console
   console.error(err.stack);
-  res.status(500).send({ error: 'Something went wrong!' });
-});
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  if (!res.headersSent) {
-    res.status(500).send({ error: 'Something went wrong!' });
+  // If headers have already been sent, do not attempt to send a response again
+  if (res.headersSent) {
+    return next(err);
   }
+
+  // Send a generic error response
+  res.status(500).json({ error: 'An unexpected error occurred. Please try again later.' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
